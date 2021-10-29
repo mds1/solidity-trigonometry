c3879827364aa05c99c5f9b1742fb9b0818cb1d3
Matt Solomon <matt@mattsolomon.dev>
2021-10-29 16:01:45 -0700

    refactor: modify sin/cos so inputs are in radians and outputs between -1 and 1

--- src/Trigonometry.t.sol
+++ src/Trigonometry.t.sol
@@ -15,7 +15,23 @@ contract TestTrigonometry {
 }
 
 contract TrigonometryTest is DSTest {
-  TestTrigonometry internal trig;
+  TestTrigonometry trig;
+
+  uint256 constant PI = 3141592653589793238; // pi as an 18 decimal value (wad)
+  int256 constant TOL = 1; // values are denominated in 1e18, so this is the smallest nonzero tolerance
+
+  function abs(int256 a) internal pure returns (int256) {
+    return a >= 0 ? a : -a;
+  }
+
+  function assertApproxEq(int256 a, int256 b, int256 abstol) internal {
+      if (abs(a - b) > abstol) {
+          emit log("Error: a ~= b not satisfied [uint]");
+          emit log_named_int("  Expected", b);
+          emit log_named_int("    Actual", a);
+          fail();
+      }
+    }
 
   function setUp() public {
     trig = new TestTrigonometry();
@@ -31,33 +47,33 @@ contract TrigonometryTest is DSTest {
 
   function testSin() public {
     // Angles within 0 <= x <= 2pi = 16384
-    assertEq(trig.sin(0),      0);     // 0
-    assertEq(trig.sin(4096),   32767); // pi/2
-    assertEq(trig.sin(8192),   0);     // pi
-    assertEq(trig.sin(12288), -32767); // 3pi/2
-    assertEq(trig.sin(16384),  0);     // 2pi
+    assertApproxEq(trig.sin(0),           0,    TOL); // 0
+    assertApproxEq(trig.sin(PI/2),        1e18, TOL); // pi/2
+    assertApproxEq(trig.sin(PI),          0,    TOL); // pi
+    assertApproxEq(trig.sin(3 * PI / 2), -1e18, TOL); // 3pi/2
+    assertApproxEq(trig.sin(2 * PI),      0,    TOL); // 2pi
     
     // Angles outside that range that must be wrapped
-    assertEq(trig.sin(1e18 + 0),      0);     // 0
-    assertEq(trig.sin(1e18 + 4096),   32767); // pi/2
-    assertEq(trig.sin(1e18 + 8192),   0);     // pi
-    assertEq(trig.sin(1e18 + 12288), -32767); // 3pi/2
-    assertEq(trig.sin(1e18 + 16384),  0);     // 2pi
+    assertApproxEq(trig.sin(1e18 * 2 * PI + 0),           0,    TOL); // 0
+    assertApproxEq(trig.sin(1e18 * 2 * PI + PI/2),        1e18, TOL); // pi/2
+    assertApproxEq(trig.sin(1e18 * 2 * PI + PI),          0,    TOL); // pi
+    assertApproxEq(trig.sin(1e18 * 2 * PI + 3 * PI / 2), -1e18, TOL); // 3pi/2
+    assertApproxEq(trig.sin(1e18 * 2 * PI + 2 * PI),      0,    TOL); // 2pi
   }
 
   function testCos() public {
-    // Angles within 0 <= x <= 2pi = 16384
-    assertEq(trig.cos(0),      32767);  // 0
-    assertEq(trig.cos(4096),   0);      // pi/2
-    assertEq(trig.cos(8192),  -32767);  // pi
-    assertEq(trig.cos(12288),  0);      // 3pi/2
-    assertEq(trig.cos(16384),  32767);  // 2pi
+    // Angles within 0 <= x <= 2pi = 2 * PI
+    assertApproxEq(trig.cos(0),           1e18, TOL); // 0
+    assertApproxEq(trig.cos(PI/2),        0,    TOL); // pi/2
+    assertApproxEq(trig.cos(PI),         -1e18, TOL); // pi
+    assertApproxEq(trig.cos(3 * PI / 2),  0,    TOL); // 3pi/2
+    assertApproxEq(trig.cos(2 * PI),      1e18, TOL); // 2pi
     
     // Angles outside that range that must be wrapped
-    assertEq(trig.cos(1e18 + 0),      32767);  // 0
-    assertEq(trig.cos(1e18 + 4096),   0);      // pi/2
-    assertEq(trig.cos(1e18 + 8192),  -32767);  // pi
-    assertEq(trig.cos(1e18 + 12288),  0);      // 3pi/2
-    assertEq(trig.cos(1e18 + 16384),  32767);  // 2pi
+    assertApproxEq(trig.cos(1e18 * 2 * PI + 0),           1e18, TOL); // 0
+    assertApproxEq(trig.cos(1e18 * 2 * PI + PI/2),        0,    TOL); // pi/2
+    assertApproxEq(trig.cos(1e18 * 2 * PI + PI),         -1e18, TOL); // pi
+    assertApproxEq(trig.cos(1e18 * 2 * PI + 3 * PI / 2),  0,    TOL); // 3pi/2
+    assertApproxEq(trig.cos(1e18 * 2 * PI + 2 * PI),      1e18, TOL); // 2pi
   }
 }
