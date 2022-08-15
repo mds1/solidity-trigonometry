f61886167a4f13b41aaa8ccefe6760c5915e20d9
Md Abid Sikder <abid@abidsikder.com>
2022-08-15 15:29:38 -0400

    feat: properly handle 18-width fixed decimal point numbers in the fuzz verify script

--- test/arcsin_fuzz_verify.jl
+++ test/arcsin_fuzz_verify.jl
@@ -1,3 +1,12 @@
 #!julia
+input = parse(Int64, ARGS[1])
 
-println(asin(parse(Int64, ARGS[1])))
+fractional_part= mod(input, 1e18)
+whole_part = input - fractional_part
+
+fractional_part = fractional_part/1e18
+whole_part = whole_part/1e18
+
+x = whole_part + fractional_part
+
+println(asin(x))
